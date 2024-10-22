96d3c24d50351d2f06e4af823f3834f9e8e438e1
boogie1989 <serhiibuhai.work@gmail.com>
2024-10-22 12:52:54 +0300

    save

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,59 +1,55 @@
 const FORM_FEEDBACK_KEY = 'feedback-form-state';
 
-const formData = {
+const defaultFormData = {
   email: '',
   message: '',
 };
 
-const resetFormData = data => {
-  Object.keys(data).forEach(key => {
-    data[key] = '';
-  });
+const resetFormData = () => {
+  return { ...defaultFormData };
 };
 
 const getFormData = () => {
-  const data = JSON.parse(localStorage.getItem(FORM_FEEDBACK_KEY));
-  if (!data) return formData;
-
-  Object.keys(data).forEach(key => {
-    formData[key] = data[key];
-  });
-
-  return formData;
+  return (
+    JSON.parse(localStorage.getItem(FORM_FEEDBACK_KEY)) || {
+      ...defaultFormData,
+    }
+  );
 };
 
-const isFormDataIsValid = () => {
-  const data = getFormData();
-  return Object.values(data).every(value => value);
+const isFormDataValid = formData => {
+  return Object.values(formData).every(Boolean);
 };
 
-const updateFormData = feedbackForm => {
-  const data = getFormData();
-  Object.keys(data).forEach(key => {
-    feedbackForm[key].value = data[key];
+const updateFormData = (feedbackForm, formData) => {
+  Object.keys(formData).forEach(key => {
+    if (feedbackForm.elements[key]) {
+      feedbackForm.elements[key].value = formData[key];
+    }
   });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   const feedbackForm = document.querySelector('.feedback-form');
+  let formData = getFormData();
+
+  updateFormData(feedbackForm, formData);
 
   feedbackForm.addEventListener('input', event => {
     formData[event.target.name] = event.target.value.trim();
     localStorage.setItem(FORM_FEEDBACK_KEY, JSON.stringify(formData));
   });
 
-  updateFormData(feedbackForm);
-
   feedbackForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    if (isFormDataIsValid()) {
+    if (isFormDataValid(formData)) {
       console.log(formData);
       localStorage.removeItem(FORM_FEEDBACK_KEY);
       feedbackForm.reset();
-      resetFormData(formData);
+      formData = resetFormData();
     } else {
-      alert('Fill please all fields');
+      alert('Please fill in all fields');
     }
   });
 });
